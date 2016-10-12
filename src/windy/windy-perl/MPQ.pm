1dc119af4360b02a34b5bea0fac84b24085a919c
ThisTusooa <tusooa@vista.aero>
2016-10-12 22:52:09 +0800

    ...and the missing pm...

---
+++ src/windy/windy-perl/MPQ.pm
@@ -0,0 +1,12 @@
+package MPQ;
+
+require Exporter;
+require DynaLoader;
+use strict;
+our $VERSION = '0.1';
+our @ISA = qw(Exporter DynaLoader);
+our @EXPORT = qw();
+our @EXPORT_OK = qw//;
+
+bootstrap MPQ;
+1;
