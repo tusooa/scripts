ef90953df224cc062caf1fa6e404e9d8699adf01
ThisTusooa <tusooa@vista.aero>
2016-11-06 18:17:37 +0800

    multi-args

--- src/windy/windy.cpp
+++ src/windy/windy.cpp
@@ -42,9 +42,13 @@ int main() {
     server.resource["^/api/call$"]["POST"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
         try {
             ptree pt;
+            //set<string> args;
             read_json(request->content, pt);
-
-            string name=pt.get<string>("func")+"("+pt.get<string>("arg")+")";
+            string name=pt.get<string>("func")+"(";
+            for (boost::property_tree::ptree::value_type &v : pt.get_child("args")) {
+              name += (v.second.data()) + ", ";
+            }
+            name += ")";
 
             *response << "HTTP/1.1 200 OK\r\n"
                       << "Content-Type: application/json\r\n"
@@ -55,28 +59,7 @@ int main() {
             *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
         }
     };
-
-    /*    //GET-example for the path /match/[number], responds with the matched string in path (number)
-    //For instance a request GET /match/123 will receive: 123
-    server.resource["^/match/([0-9]+)$"]["GET"]=[&server](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
-        string number=request->path_match[1];
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
-    };
-    
-    //Get example simulating heavy work in a separate thread
-    server.resource["^/work$"]["GET"]=[&server](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
-        thread work_thread([response] {
-            this_thread::sleep_for(chrono::seconds(5));
-            string message="Work done";
-            *response << "HTTP/1.1 200 OK\r\nContent-Length: " << message.length() << "\r\n\r\n" << message;
-        });
-        work_thread.detach();
-    };*/
-    
-    //Default GET-example. If no other matches, this anonymous function will be called. 
-    //Will respond with content in the web/-directory, and its subdirectories.
-    //Default file: index.html
-    //Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
+  
     server.default_resource["GET"]=[&server](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
       string content="Could not open path "+request->path;
       *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
